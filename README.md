b42a1177fa43ef6136dc14cf5b6e304df9a54132
manasux <amr13mk@gmail.com>
2021-09-16 09:26:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-synbh8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-synbh8)
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

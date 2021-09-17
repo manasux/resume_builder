77af2212f0f51ef584388bed2ee83c5834c7db4d
manasux <amr13mk@gmail.com>
2021-09-16 21:11:19 -0700

    added company name

--- src/components/Second/Experience.vue
+++ src/components/Second/Experience.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="experience">
+    <!-- Experience Title -->
     <div class="experience_title">
       <span>Experiences</span>
+    </div>
+    <!-- Experience Description -->
+    <div class="experience_desc">
+      <div class="experience_desc_title">
+        <span>LearnCodeOnline</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-.experience{
+.experience {
   height: auto;
   width: 70%;
-  border-right: 1px solid #333; 
+  border-right: 1px solid #333;
 }
-.experience_title { 
+.experience_title {
   margin-top: 30px;
   margin-left: 30px;
   font-size: 28px;
   font-family: var(--font);
   color: #cb570c;
+}
+.experience_desc {
+  width: 100%;
+  margin-top: 10px;
+  margin-left: 30px;
+}
+.experience_desc_title > span {
+  font-family: var(--font_sans);
+  font-weight: 600;
 }
 </style>

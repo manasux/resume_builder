ca3d9c0b0d2ae502528d75654949f03821588e0b
manasux <amr13mk@gmail.com>
2021-09-18 12:01:42 +0530

    added emitbus

--- src/components/Input/HeaderInput.vue
+++ src/components/Input/HeaderInput.vue
@@ -5,23 +5,28 @@
       <!-- Input Name -->
       <div class="input_name_div flex_input">
         <label for="name">Name</label>
-        <input
-          type="text"
-          class="name_input"
-          name="name"
-          v-model="resume_name"
-        />
+        <input type="text" class="name_input" name="name" v-model="new_input" />
       </div>
+      <button @click="add_name_btn" class="btn_add">Add Name</button>
     </div>
+    <p>{{ resume_name }}</p>
   </div>
 </template>
 
 <script>
+import { EventBus } from './event-bus.js';
 export default {
   data() {
     return {
       resume_name: 'Manas',
+      new_input: '',
     };
+  },
+  methods: {
+    add_name_btn() {
+      this.resume_name = this.new_input;
+      EventBus.$emit('clicked-event', this.Count);
+    },
   },
 };
 </script>

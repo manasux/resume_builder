23f8b6b4d8ffb1785da0bb80eb62f80d3bf55e02
manasux <amr13mk@gmail.com>
2021-09-20 09:24:03 +0530

    added changing name icon  and toggle

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -5,6 +5,9 @@
       <span class="name_second">
         {{ name }}
       </span>
+      <button @click="changenameCounter()" class="btn btn_icon">
+        <i class="fas fa-pen"></i>
+      </button>
     </div>
     <!-- phone number -->
     <div class="phone">
@@ -19,7 +22,7 @@
   <!-- ------------------------------------- -->
 
   <!-- Input -->
-  <div class="input_main_header">
+  <div v-if="counter == 1 && active" class="input_main_header">
     <div class="input_header">
       <div class="name">
         <label for="name">Name</label>
@@ -36,6 +39,8 @@ export default {
     return {
       name: 'Manas Mishra',
       new_input: '',
+      counter: 0,
+      active: false,
     };
   },
   methods: {
@@ -43,16 +48,61 @@ export default {
       this.name = this.new_input;
       this.new_input = '';
     },
+    changenameCounter() {
+      this.counter = 1;
+      this.active = !this.active;
+    },
   },
 };
 </script>
 
 <style>
-@import './Header.css';
-
+.header {
+  width: 100%;
+  height: 25%;
+  background-color: #333e50;
+}
+.flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.name {
+  color: white;
+  font-family: var(--font);
+  font-size: 35px;
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: center;
+  align-items: center;
+}
+.name_second {
+  color: #fff;
+}
+.phone_number {
+  color: #fff;
+  font-size: 15px;
+  font-family: var(--font_roboto);
+}
+.email_desc {
+  color: #fff;
+  font-family: var(--font_roboto);
+}
 .input_header {
   width: 60%;
   display: flex;
   height: 60%;
+}
+.btn_icon {
+  width: 20px;
+  height: 20px;
+  margin-left: 20px;
+  border: 0.2px solid black;
+  border-radius: 5px;
+  background: #fc30ef;
+  color: #fff;
+  font-size: 10px;
+  cursor: pointer;
 }
 </style>
